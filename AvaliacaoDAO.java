d385bb2cbde54ad8df026edb6da1c72391e1701e
Lucas Rohr Carreño <40070210+LucasRohr@users.noreply.github.com>
2018-11-01 18:04:31 -0300

    Adding a interface for Avaliacao DAO
    For code standardization.

---
+++ AvaliacaoDAO.java
@@ -0,0 +1,21 @@
+package com.melodiam.persistencia;
+
+import java.util.List;
+
+import com.melodiam.model.Avaliacao;
+import com.melodiam.model.Usuario;
+
+
+public interface AvaliacaoDAO {
+	
+	public Avaliacao cadastrarAvaliacao(Avaliacao avaliacao);
+	public void editarAvaliacao(Avaliacao avaliacao);
+	public void excluirAvaliacao(long id);
+	
+	public Avaliacao buscarPorId(long id);
+	public List<Avaliacao> buscarTodas();
+	public List<Avaliacao> buscarPorUsuario(Usuario usuario); 
+	public float calcularMedia();
+	
+
+}

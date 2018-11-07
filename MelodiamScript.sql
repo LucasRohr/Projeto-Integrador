b5e35fab68db8f8ae7a3bef4d84a99ce489674e7
Lucas Rohr Carreño <40070210+LucasRohr@users.noreply.github.com>
2018-11-07 09:43:53 -0200

    SQL file for the database of the project

---
+++ MelodiamScript.sql
@@ -0,0 +1,75 @@
+CREATE DATABASE melodiam;
+USE melodiam;
+
+CREATE TABLE Usuario (
+	id_usuario BIGINT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    login_usuario VARCHAR(20) NOT NULL UNIQUE,
+    senha_usuario VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE Album (
+	id_spotify VARCHAR(22) NOT NULL UNIQUE, -- É String(VARCHAR) porque pegaremos o ID do BD do Spotify. EX: 6rqhFgbbKwnb9MLmUQDhG6
+    id_album BIGINT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE Avaliacao_Album (
+	id_avaliacao_album BIGINT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    avaliacao_album FLOAT NOT NULL,
+    id_usuario BIGINT NOT NULL,
+    id_album BIGINT NOT NULL,
+    
+    FOREIGN KEY(id_usuario) REFERENCES Usuario(id_usuario),
+    FOREIGN KEY(id_album) REFERENCES Album(id_album)
+);
+
+CREATE TABLE Comentario (
+	id_comentario BIGINT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    texto VARCHAR(200) NOT NULL,
+    data_hora DATETIME NOT NULL,
+    id_usuario BIGINT NOT NULL,
+    id_album BIGINT NOT NULL,
+    
+    FOREIGN KEY(id_usuario) REFERENCES Usuario(id_usuario),
+    FOREIGN KEY(id_album) REFERENCES Album(id_album)
+);
+
+CREATE TABLE Lista (
+	id_lista BIGINT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    nome_lista VARCHAR(25) NOT NULL,
+    descricao_lista VARCHAR(100) NOT NULL,
+    id_usuario BIGINT NOT NULL,
+    
+    FOREIGN KEY(id_usuario) REFERENCES Usuario(id_usuario)
+);
+
+CREATE TABLE Avaliacao_Lista (
+	id_avaliacao_lista BIGINT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    avaliacao_lista FLOAT NOT NULL,
+    id_usuario BIGINT NOT NULL,
+    id_lista BIGINT NOT NULL,
+    
+    FOREIGN KEY(id_usuario) REFERENCES Usuario(id_usuario),
+    FOREIGN KEY(id_lista) REFERENCES Lista(id_lista)
+);
+
+CREATE TABLE Album_Lista (
+	id_album_lista BIGINT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+	id_album BIGINT NOT NULL, 
+	id_lista BIGINT NOT NULL,
+    
+    FOREIGN KEY(id_album) REFERENCES Album(id_album),
+    FOREIGN KEY(id_lista) REFERENCES Lista(id_lista)    
+);
+
+CREATE TABLE Amizade (
+	id_amizade BIGINT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    status_amizade BOOLEAN NOT NULL,
+    
+    id_usuario1 BIGINT NOT NULL,
+    FOREIGN KEY(id_usuario1) REFERENCES Usuario(id_usuario),
+    
+    id_usuario2 BIGINT NOT NULL,
+    FOREIGN KEY(id_usuario2) REFERENCES Usuario(id_usuario)
+);
+
+
